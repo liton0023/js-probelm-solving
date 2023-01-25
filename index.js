a7ac93e4cd4f5e9805ee9f3cd06b0fcffa451eee
Md Liton Mahmud <litonmahmud0023@gmail.com>
2023-01-26 02:35:56 +0600

    add probelm

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ console.log(result);
  }
  oilPrice(4,2,3);
 
- 
+
 // public bus transport cost
 
  function publicBusFare(public){
@@ -240,3 +240,29 @@ else{
 const bestFr= isBestFriend(friend1,friend2);
 console.log(bestFr);
  
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+
+String.prototype.toJadenCase = function () {
+    var words = this.split(' ');
+  
+    for(var i = 0, wordsLen = words.length; i < wordsLen; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+  
+    return words.join(' ');
+  };
+
+
+//   Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+
+//my example
+function descendingOrder(n){
+    var digits = (""+n).split("").sort(function(a, b){return b-a});
+    digits = digits.join("");
+    return console.log( digits);
+  }
+  descendingOrder(23532);
